ec7acdc59878230b8874ed0a72dffe351e4da2a4
Sheetal <sheetalbajajgodda@gmail.com>
2025-03-02 18:54:18 +0530

    f-strings

---
+++ fstring.py
@@ -0,0 +1,10 @@
+letter="Hey my name is {} and I am from {}"
+country="India"
+name="Sheetal"
+
+print(letter.format(name,country))
+val = 'Geeks'  
+print(f"{val}for{val} is a portal for {val}.")  
+name = 'Tushar'  
+age = 23  
+print(f"Hello, My name is {name} and I'm {age} years old.")
