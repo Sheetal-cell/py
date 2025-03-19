d275eac314a9e2cf5de52bfda48fdc67d38eee21
Sheetal <sheetalbajajgodda@gmail.com>
2025-03-19 13:45:56 +0530

    code

--- library management system.py
+++ library management system.py
@@ -1,1 +1,22 @@
+class Library:
+  def __init__(self):
+    self.no_of_Books = 0
+    self.books = []
+    
+  def addBook(self, book):
+    self.books.append(book)
+    self.no_of_Books = len(self.books)
 
+  def showInfo(self):
+    print(f"The library has {self.no_of_Books} books. The books are")
+    for book in self.books:
+      print(book)
+
+
+obj = Library()
+obj.addBook("Harry Potter1")
+obj.addBook("Harry Potter2")
+obj.addBook("Harry Potter3")
+obj.showInfo()
+    
+  
